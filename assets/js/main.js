a3ffc1b504c4b3aeb1b44dc6b1142d614da60d14
SuperCoder <propscode254@gmail.com>
2023-09-15 11:39:14 +0200

    add all variables

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+// Save all variables
+
+// inputs
+
+let bill = document.querySelector('[data-js="bill"]');
+console.log({ bill });
+let people = document.querySelector('[data-js="people"]');
+console.log({ people });
+let optionBad = document.querySelector('[data-js="bad-service"]');
+console.log({ optionBad });
+let optionMiddle = document.querySelector('[data-js="middle-service"]');
+console.log({ optionMiddle });
+let optionGood = document.querySelector('[data-js="good-service"]');
+console.log({ optionGood });
+
+// outputs
+
+let totalTip = document.querySelector('[data-js="total-tip"]');
+console.log({ totalTip });
+let totalBill = document.querySelector('[data-js="total-bill"]');
+console.log({ totalBill });
+let partialTip = document.querySelector('[data-js="partial-tip"]');
+console.log({ partialTip });
+let partialBill = document.querySelector('[data-js="partial-bill"]');
+console.log({ partialBill });
+
+// extra : change bck - design
+
+let wrapper = document.querySelector('[data-js="wrapper"]');
+console.log({ wrapper });
